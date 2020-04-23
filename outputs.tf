a0d4b1c0a2832ee1055fdec641bb17a9ae745c4e
bainss <surj@polarpoint.io>
2020-04-23 17:11:30 +0100

    rewrite

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "public_key" {
-  value       = tls_private_key.ssh_key_pair.public_key_ssh
+  value       = tls_private_key.ssh.public_key_ssh
   description = "Content of the generated public key"
 }
